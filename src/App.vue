c5c9a2cb3a1645d025bd3a967e0a573f8cff6e3c
Samuel <adetona.samuel@e4email.net>
2023-01-31 15:32:59 +0000

    add animation

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
-  <router-view />
+    <div>
+    <router-view v-slot=“{ Component, route }“>
+     <transition
+        :name=“route.meta?.transition || ‘slide-fade’”
+        :duration=“{ enter: 300, leave: 500 }”
+        mode="out-in"
+      >
+        <component :is=“Component” :key=“route?.path + route?.name” />
+      </transition>
+    </router-view>
+    </div>
 </template>
 
+<script lang=“ts”>
+import { defineComponent } from 'vue';
+export default defineComponent({
+
+});
+</script>
+
 <style lang="scss">
-// #app {
-//   font-family: Avenir, Helvetica, Arial, sans-serif;
-//   -webkit-font-smoothing: antialiased;
-//   -moz-osx-font-smoothing: grayscale;
-//   text-align: center;
-//   color: #2c3e50;
-// }
-
-// nav {
-//   padding: 30px;
-
-//   a {
-//     font-weight: bold;
-//     color: #2c3e50;
-
-//     &.router-link-exact-active {
-//       color: #42b983;
-//     }
-//   }
-// }
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateY(10px);
+  opacity: 0;
+}
 </style>
--- src/modules/dummy/Form.vue
+++ src/modules/dummy/Form.vue
@@ -1,42 +1,50 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <div style="margin: 20px;" class="guy">
+  <div class="guy">
     <el-form
       :label-position="labelPosition"
       label-width="100px"
-      :model="formLabelAlign"
+      :model="dataObject"
       style="max-width: 460px"
+      @submit.prevent="handleSubmit"
     >
       <el-form-item label="Indicator">
-        <el-input v-model="formLabelAlign.indicator" />
+        <el-input v-model.trim="dataObject.indicator" />
       </el-form-item>
       <el-form-item label="Datasource">
-        <el-input v-model="formLabelAlign.datasource" />
+        <el-input v-model.trim="dataObject.datasource" />
       </el-form-item>
-      <el-button type="success" class="button" plain>Success</el-button>
+      <el-button type="success" class="button" plain
+        >Success</el-button
+      >
     </el-form>
-    </div>
-  </template>
-  
-  <script lang="ts" setup>
-  import { reactive, ref } from 'vue'
-//   import 
-  
-  const labelPosition = ref('right')
-  
-  const formLabelAlign = reactive({
-    indicator: '',
-    datasource: '',
-  })
-  </script>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { reactive, ref } from "vue";
+import AuthService from "@/utils/Service";
+
+const labelPosition = ref("right");
+
+const dataObject = reactive({
+  indicator: "",
+  datasource: "",
+});
+
+const handleSubmit = async () => {
+  const response = await AuthService.submitData(dataObject);
+  console.log(response);
+};
+</script>
 <style>
 .guy {
-    min-height: 80vh;
+  min-height: 80vh;
   display: flex;
   align-items: center;
   justify-content: center;
 }
 .button {
-    float: right;
+  float: right;
 }
 </style>
