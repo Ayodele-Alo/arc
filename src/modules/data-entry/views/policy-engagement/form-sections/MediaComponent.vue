748e7f40a4501a8e8b4ad15294afba141b07205a
e4ewebdev <webdev@e4email.net>
2023-03-16 16:53:34 +0100

    policy engagement section ui completed and funtionality framework started

--- src/modules/data-entry/views/policy-engagement/form-sections/MediaComponent.vue
+++ src/modules/data-entry/views/policy-engagement/form-sections/MediaComponent.vue
@@ -7,60 +7,83 @@
           means of verification
         </p>
       </div>
-      <table class="table table-bordered">
-        <tbody>
-          <tr>
-            <th class="header-width" scope="col">
-              <span class="row-header">
-                Interviews, newspapers, newsletters, magazines, social media
-                <p class="row-sub-header">
-                  (E.g. Twitter, Facebook, Youtube etc.)
-                </p>
-              </span>
-            </th>
 
-            <th class="header-width" scope="col">
-              <span class="row-header">
-                Analytics
-                <p class="row-sub-header">
-                  (Brief of what the campaign was about)
-                </p>
-              </span>
-            </th>
-            <th class="header-width" scope="col">
-              <span class="row-header">
-                Campaign conducted
-                <p class="row-sub-header">
-                  (Number of views, shares, bounce rate, mentions, comments and
-                  likes etc.)
-                </p>
-              </span>
-            </th>
-          </tr>
-          <tr>
-            <th class="row-header">
-              <div class="row">
-                <div class="col text-start">
-                  <textarea
+      <div class="outer-wrapper">
+        <div class="table-wrapper">
+          <table class="table table-bordered mt-5">
+            <tbody>
+              <tr>
+                <td>
+                  <span class="row-header">
+                    Interviews, newspapers, newsletters, magazines, social media
+                    <p class="row-sub-header">
+                      (E.g. Twitter, Facebook, Youtube etc.)
+                    </p>
+                  </span>
+                </td>
+                <td>
+                  <span class="row-header">
+                    Analytics
+                    <p class="row-sub-header">
+                      (Brief of what the campaign was about)
+                    </p>
+                  </span>
+                </td>
+                <td>
+                  <span class="row-header">
+                    Campaign conducted
+                    <p class="row-sub-header">
+                      (Number of views, shares, bounce rate, mentions, comments
+                      and likes etc.)
+                    </p>
+                  </span>
+                </td>
+              </tr>
+              <tr v-for="(item, index) in media_links_form" :key="index">
+                <td>
+                  <div class="d-flex align-items-center">
+                    <p class="mt-3">{{ index + 1 }}.</p>
+                    <input
+                      placeholder="Type Here"
+                      type="text"
+                      v-model="item.social_media_links"
+                    />
+                  </div>
+                </td>
+                <td>
+                  <input
                     placeholder="Type Here"
-                    name=""
-                    id=""
-                    cols="8"
-                    rows="5"
-                  ></textarea>
-                </div>
-              </div>
-            </th>
-            <td></td>
-            <td></td>
-          </tr>
-        </tbody>
-      </table>
-      <p class="add-more">+ Add more</p>
+                    type="text"
+                    v-model="item.analytics"
+                  />
+                </td>
+
+                <td class="wrapper">
+                  <input
+                    placeholder="Type Here"
+                    type="text"
+                    v-model="item.campaign_conducted"
+                  />
+                  <div
+                    v-if="media_links_form.length > 1"
+                    @click="removeRow(item)"
+                    class="remove-icon"
+                  >
+                    x
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <p @click="addToMediaLinks()" class="add-more mx-3">+ Add more</p>
 
       <div class="text-start mt-4">
         <p class="engagement-text">Campaigns conducted</p>
       </div>
+
       <table class="table table-bordered">
         <tbody>
           <tr>
@@ -70,26 +93,30 @@
               </span>
             </th>
           </tr>
-          <tr>
-            <th class="row-header">
-              <div class="row">
-                <div class="col text-start">
-                  <textarea
-                    placeholder="Type Here"
-                    name=""
-                    id=""
-                    cols="8"
-                    rows="5"
-                  ></textarea>
-                </div>
+          <tr v-for="(item, index) in campaign_conducted_form" :key="index">
+            <td class="wrapper">
+              <div class="d-flex align-items-center">
+                <p class="mt-3">{{ index + 1 }}.</p>
+                <input
+                  placeholder="Type Here"
+                  type="text"
+                  v-model="item.campaign"
+                />
               </div>
-            </th>
+              <div
+                v-if="campaign_conducted_form.length > 1"
+                @click="removeCampaignRow(item)"
+                class="remove-icon"
+              >
+                x
+              </div>
+            </td>
           </tr>
         </tbody>
       </table>
-      <p class="add-more">+ Add more</p>
+      <p @click="addToCampaign()" class="add-more mb-4">+ Add more</p>
 
-      <table class="table table-bordered mt-4">
+      <table class="table table-bordered mt-5">
         <tbody>
           <tr>
             <th class="header-width-2" scope="col">
@@ -99,25 +126,181 @@
               </span>
             </th>
           </tr>
-          <tr>
-            <th class="row-header py-3">
-              <div class="row">
-                <div class="col text-start">
-                  <input placeholder="Type Here" type="text" />
-                </div>
+          <tr v-for="(item, index) in follow_up_conversation" :key="index">
+            <td class="wrapper">
+              <div class="d-flex align-items-center">
+                <p class="mt-3">{{ index + 1 }}.</p>
+                <input
+                  placeholder="Type Here"
+                  type="text"
+                  v-model="item.conversation"
+                />
               </div>
-            </th>
+              <div
+                v-if="follow_up_conversation.length > 1"
+                @click="removeConversationRow(item)"
+                class="remove-icon"
+              >
+                x
+              </div>
+            </td>
           </tr>
         </tbody>
       </table>
-      <p class="add-more">+ Add more</p>
+      <p @click="addToFollowUpConversation()" class="add-more">+ Add more</p>
+      <div class="d-flex justify-content-end">
+        <button @click="saveForm()" class="btn btn-outline-primary">
+          save
+        </button>
+      </div>
     </div>
   </div>
 </template>
 
-<script lang="ts">
+<script>
 export default {
   name: "MediaComponents",
+
+  data() {
+    return {
+      error: true,
+      media_links_form: [
+        {
+          id: 1,
+          social_media_links: "",
+          analytics: "",
+          campaign_conducted: "",
+        },
+      ],
+      campaign_conducted_form: [
+        {
+          id: 1,
+          campaign: "",
+        },
+      ],
+
+      follow_up_conversation: [
+        {
+          id: 1,
+          conversation: "",
+        },
+      ],
+    };
+  },
+
+  methods: {
+    addToMediaLinks() {
+      this.media_links_form.push({
+        id: new Date().getTime(),
+        social_media_links: "",
+        analytics: "",
+        campaign_conducted: "",
+      });
+    },
+
+    addToCampaign() {
+      this.campaign_conducted_form.push({
+        id: new Date().getTime(),
+        campaign: "",
+      });
+    },
+
+    addToFollowUpConversation() {
+      this.follow_up_conversation.push({
+        id: new Date().getTime(),
+        conversation: "",
+      });
+    },
+
+    removeConversationRow(rowItem) {
+      if (this.follow_up_conversation.length > 1) {
+        this.follow_up_conversation = this.follow_up_conversation.filter(
+          (item) => item.id !== rowItem.id
+        );
+      }
+    },
+
+    removeCampaignRow(rowItem) {
+      if (this.campaign_conducted_form.length > 1) {
+        this.campaign_conducted_form = this.campaign_conducted_form.filter(
+          (item) => item.id !== rowItem.id
+        );
+      }
+    },
+
+    removeRow(rowItem) {
+      if (this.media_links_form.length > 1) {
+        this.media_links_form = this.media_links_form.filter(
+          (item) => item.id !== rowItem.id
+        );
+      }
+    },
+
+    saveForm() {
+      this.media_links_form.forEach((item) => {
+        for (const property in item) {
+          // console.log(`${property}: ${object[property]}`);
+          if (item[property] === "") {
+            this.error = true;
+            alert(`please ${property} cannot be blank `);
+            return;
+          } else {
+            this.error = false;
+            // console.log(this.engagements_form);
+          }
+        }
+      });
+
+      if (this.error) {
+        return;
+      }
+
+      this.campaign_conducted_form.forEach((item) => {
+        for (const property in item) {
+          // console.log(`${property}: ${object[property]}`);
+          if (item[property] === "") {
+            this.error = true;
+            alert(`please ${property} cannot be blank `);
+            return;
+          } else {
+            this.error = false;
+            // console.log(this.engagements_form);
+          }
+        }
+      });
+      if (this.error) {
+        return;
+      }
+
+      this.follow_up_conversation.forEach((item) => {
+        for (const property in item) {
+          // console.log(`${property}: ${object[property]}`);
+          if (item[property] === "") {
+            this.error = true;
+            alert(`please ${property} cannot be blank `);
+            return;
+          } else {
+            this.error = false;
+            // console.log(this.engagements_form);
+          }
+        }
+      });
+      if (this.error) {
+        return;
+      }
+      this.formatData();
+    },
+
+    formatData() {
+      const data = {
+        media_links_form: this.media_links_form,
+        campaign_conducted_form: this.campaign_conducted_form,
+        follow_up_conversation: this.follow_up_conversation,
+      };
+
+      console.log(data);
+    },
+  },
 };
 </script>
 
@@ -138,7 +321,7 @@ export default {
 }
 .header-width-2 {
   width: 170px;
-  height: 100px;
+  height: 60px;
   text-align: left;
   padding: 4px 8px;
   padding: 15px;
@@ -201,5 +384,39 @@ input {
 }
 table {
   background-color: white;
+}
+
+.table-wrapper::-webkit-scrollbar {
+  width: 20px;
+  height: 10px;
+}
+
+.table-wrapper::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 5px grey;
+  border-radius: 10px;
+}
+
+.table-wrapper::-webkit-scrollbar-thumb {
+  background: #70707069;
+  border-radius: 10px;
+}
+.wrapper {
+  position: relative;
+}
+.remove-icon {
+  position: absolute;
+  bottom: -5px;
+  right: -5px;
+  background-color: #f75d59;
+  /* padding: 2px; */
+  border-radius: 50%;
+  width: 20px;
+  height: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  cursor: pointer;
+  line-height: 10px;
 }
 </style>
--- src/modules/data-entry/views/policy-engagement/form-sections/PolicyStrategies.vue
+++ src/modules/data-entry/views/policy-engagement/form-sections/PolicyStrategies.vue
@@ -46,31 +46,73 @@
               <span class="row-header"> Venue/country </span>
             </th>
           </tr>
-          <tr>
-            <th class="row-header py-3">
-              <div class="row">
-                <div class="col text-start">
-                  <input placeholder="Type Here" type="text" />
-                </div>
+          <tr v-for="(item, index) in capacity_building" :key="index">
+            <th class="row-header py-1">
+              <div class="d-flex align-items-center">
+                <p class="mt-3">{{ index + 1 }}.</p>
+                <input
+                  placeholder="Type Here"
+                  type="text"
+                  v-model="item.themes_and_signature"
+                />
               </div>
             </th>
             <td>
               <div class="gender">
                 <div class="male">
-                  <input placeholder="Type Here" type="text" />
+                  <input
+                    placeholder="Type Here"
+                    type="text"
+                    v-model="item.number_of_partispant.male"
+                  />
                 </div>
                 <div class="female">
-                  <input placeholder="Type Here" type="text" />
+                  <input
+                    placeholder="Type Here"
+                    type="text"
+                    v-model="item.number_of_partispant.female"
+                  />
                 </div>
               </div>
             </td>
-            <td></td>
-            <td></td>
-            <td></td>
+            <td>
+              <div class="p-2">
+                <input
+                  placeholder="Type Here"
+                  type="text"
+                  v-model="item.purpose_of_project"
+                />
+              </div>
+            </td>
+            <td>
+              <div class="p-2">
+                <input
+                  placeholder="Type Here"
+                  type="text"
+                  v-model="item.project_link"
+                />
+              </div>
+            </td>
+            <td class="wrapper">
+              <div class="p-2">
+                <input
+                  placeholder="Type Here"
+                  type="text"
+                  v-model="item.venue"
+                />
+              </div>
+              <div
+                v-if="capacity_building.length > 1"
+                @click="removeCapacity(item)"
+                class="remove-icon"
+              >
+                x
+              </div>
+            </td>
           </tr>
         </tbody>
       </table>
-      <p class="add-more">+ Add more</p>
+      <p @click="addToCapacity()" class="add-more">+ Add more</p>
 
       <div class="text-start mt-5">
         <p class="engagement-text">
@@ -112,38 +154,213 @@
               <span class="row-header"> Venue/country </span>
             </th>
           </tr>
-          <tr>
-            <th class="row-header py-3">
-              <div class="row">
-                <div class="col text-start">
-                  <input placeholder="Type Here" type="text" />
-                </div>
+          <tr
+            v-for="(item, index) in capacity_strengthened_activities"
+            :key="index"
+          >
+            <th class="row-header py-1">
+              <div class="d-flex align-items-center">
+                <p class="mt-3">{{ index + 1 }}.</p>
+                <input
+                  placeholder="Type Here"
+                  type="text"
+                  v-model="item.themes_and_signature"
+                />
               </div>
             </th>
             <td>
               <div class="gender">
                 <div class="male">
-                  <input placeholder="Type Here" type="text" />
+                  <input
+                    placeholder="Type Here"
+                    type="text"
+                    v-model="item.number_of_partispant.male"
+                  />
                 </div>
                 <div class="female">
-                  <input placeholder="Type Here" type="text" />
+                  <input
+                    placeholder="Type Here"
+                    type="text"
+                    v-model="item.number_of_partispant.female"
+                  />
                 </div>
               </div>
             </td>
-            <td></td>
-            <td></td>
-            <td></td>
+            <td>
+              <div class="p-2">
+                <input
+                  placeholder="Type Here"
+                  type="text"
+                  v-model="item.purpose_of_project"
+                />
+              </div>
+            </td>
+            <td>
+              <div class="p-2">
+                <input
+                  placeholder="Type Here"
+                  type="text"
+                  v-model="item.project_link"
+                />
+              </div>
+            </td>
+            <td class="wrapper">
+              <div class="p-2">
+                <input
+                  placeholder="Type Here"
+                  type="text"
+                  v-model="item.venue"
+                />
+              </div>
+              <div
+                v-if="capacity_strengthened_activities.length > 1"
+                @click="removeCapacityStrengthened(item)"
+                class="remove-icon"
+              >
+                x
+              </div>
+            </td>
           </tr>
         </tbody>
       </table>
-      <p class="add-more">+ Add more</p>
+      <p @click="addToCapacityStrengthened()" class="add-more">+ Add more</p>
+
+      <div class="d-flex justify-content-end">
+        <button @click="saveForm()" class="btn btn-outline-primary">
+          save
+        </button>
+      </div>
     </div>
   </div>
 </template>
 
-<script lang="ts">
+<script>
 export default {
   name: "PolicyStrategies",
+
+  data() {
+    return {
+      capacity_building: [
+        {
+          id: 1,
+          themes_and_signature: "",
+          number_of_partispant: {
+            male: null,
+            female: null,
+          },
+          purpose_of_project: "",
+          project_link: "",
+          venue: "",
+        },
+      ],
+      capacity_strengthened_activities: [
+        {
+          id: 1,
+          themes_and_signature: "",
+          number_of_partispant: {
+            male: null,
+            frmaile: null,
+          },
+          purpose_of_project: "",
+          project_link: "",
+          venue: "",
+        },
+      ],
+    };
+  },
+  methods: {
+    addToCapacity() {
+      this.capacity_building.push({
+        id: new Date().getTime(),
+        themes_and_signature: "",
+        number_of_partispant: {
+          male: null,
+          frmaile: null,
+        },
+        purpose_of_project: "",
+        project_link: "",
+        venue: "",
+      });
+    },
+
+    removeCapacity(rowItem) {
+      if (this.capacity_building.length > 1) {
+        this.capacity_building = this.capacity_building.filter(
+          (item) => item.id !== rowItem.id
+        );
+      }
+    },
+
+    addToCapacityStrengthened() {
+      this.capacity_strengthened_activities.push({
+        id: new Date().getTime(),
+        themes_and_signature: "",
+        number_of_partispant: {
+          male: null,
+          frmaile: null,
+        },
+        purpose_of_project: "",
+        project_link: "",
+        venue: "",
+      });
+    },
+
+    removeCapacityStrengthened(rowItem) {
+      if (this.capacity_strengthened_activities.length > 1) {
+        this.capacity_strengthened_activities =
+          this.capacity_strengthened_activities.filter(
+            (item) => item.id !== rowItem.id
+          );
+      }
+    },
+
+    saveForm() {
+      this.capacity_building.forEach((item) => {
+        for (const property in item) {
+          // console.log(`${property}: ${object[property]}`);
+          if (item[property] === "") {
+            this.error = true;
+            alert(`please ${property} cannot be blank `);
+            return;
+          } else {
+            this.error = false;
+            // console.log(this.engagements_form);
+          }
+        }
+      });
+
+      if (this.error) {
+        return;
+      }
+
+      this.capacity_strengthened_activities.forEach((item) => {
+        for (const property in item) {
+          // console.log(`${property}: ${object[property]}`);
+          if (item[property] === "") {
+            this.error = true;
+            alert(`please ${property} cannot be blank `);
+            return;
+          } else {
+            this.error = false;
+            // console.log(this.engagements_form);
+          }
+        }
+      });
+      if (this.error) {
+        return;
+      }
+      this.formatData();
+    },
+
+    formatData() {
+      const data = {
+        capacity_strengthened_activities: this.capacity_strengthened_activities,
+        capacity_building: this.capacity_building,
+      };
+
+      console.log(data);
+    },
+  },
 };
 </script>
 
@@ -263,7 +480,7 @@ table {
 .gender {
   height: 100px;
   display: flex;
-  margin-top: -8px;
+  /* margin-top: -8px; */
   height: 100%;
 }
 .male {
@@ -276,5 +493,23 @@ table {
   width: 50%;
   height: 50px;
   padding: 10px;
+}
+.wrapper {
+  position: relative;
+}
+.remove-icon {
+  position: absolute;
+  bottom: -5px;
+  right: -5px;
+  background-color: #f75d59;
+  padding: 2px;
+  border-radius: 50%;
+  width: 20px;
+  height: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  cursor: pointer;
 }
 </style>
