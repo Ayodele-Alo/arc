caa57c455678bbc5655341fb64faa4e999d2c4c3
usiere <akpabio98@gmail.com>
2023-03-28 15:57:01 +0100

    casing changes made to the publications component

--- src/modules/data-entry/views/publications/form-sections/journalForm.vue
+++ src/modules/data-entry/views/publications/form-sections/journalForm.vue
@@ -4,7 +4,7 @@
      <div class="main-sec">
 <span class="form-title">Journal Volume</span>
 <hr class="hr" />
-<input type="text" placeholder="Type here">
+<input type="text" placeholder="Type here" v-model="journal_form">
 
 
 </div>
@@ -14,7 +14,7 @@
  <div class="main-sec">
 <span class="form-title">Supplement Number</span>
 <hr class="hr" />
-<input type="text" placeholder="Type here">
+<input type="text" placeholder="Type here" v-model="supplement_number">
 </div>
 
 <br>
@@ -22,7 +22,7 @@
  <div class="main-sec">
 <span class="form-title">Journal Issue</span>
 <hr class="hr" />
-<input type="text" placeholder="Type here">
+<input type="text" placeholder="Type here" v-model="journal_issue">
  
 
 </div>
@@ -32,21 +32,50 @@
  <div class="main-sec">
 <span class="form-title">Journal Page</span>
 <hr class="hr" />
-<input type="text" placeholder="Type here">
+<input type="text" placeholder="Type here" v-model="journal_page">
  
 
 </div>
+
+  <div class="d-flex justify-content-end mt-4">
+      <div @click="saveForm()" class="save-icon">
+        <i class="fa fa-save fs-5 mr-2" aria-hidden="true"></i>
+        <h5>save</h5>
+      </div>
+    </div>
 </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
+import { mapActions } from "vuex";
+export default {
+  data() {
+    return {
+      journal_form: "",
+      supplement_number: "",
+      journal_issue: "",
+      journal_page: ""
+    };
+  },
 
-      }
-    }
-  }
+  methods: {
+    ...mapActions(["SAVE_DATA"]),
+
+    saveForm() {
+      const data = {
+        journal_form: this.journal_form,
+        supplement_number: this.supplement_number,
+        journal_issue: this.journal_issue,
+        journal_page: this.journal_page
+      };
+      const newItem = {
+        component: "publications",
+        item: { name: "journalForm", form: data },
+      };
+      this.SAVE_DATA(newItem);
+    },
+  },
+};
 </script>
 
 
